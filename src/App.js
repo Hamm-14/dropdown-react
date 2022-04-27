ec3da54cebf15f2bbb4bbca687ecd4ab9f02fdd7
Hammad <hammadyasub14@gmail.com>
2022-04-27 09:36:44 +0530

    add UI for dropdown

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      listItem: ["Yes", "Probable Not", "Lets Nacho", "Maza aa gaya"],
+      buttonValue: null,
+    };
+  }
+
+  render() {
+    const { buttonValue, listItem } = this.state;
+    return (
+      <div className="App">
+        <div id="dropdown-container">
+          <h1>Should you use a dropdown ?</h1>
+          <div id="dropdown-btn">
+            <div id="btn-value">{buttonValue ? buttonValue : "Select"}</div>
+            <div id="open-dropdown">
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/32/32195.png"
+                alt="open dropdown"
+              />
+            </div>
+          </div>
+
+          <div id="dropdown-list">
+            {listItem.map((item) => {
+              return <div className="list-item">{item}</div>;
+            })}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
