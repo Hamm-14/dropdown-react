6154bd882b5b8ca03ae3fcc7d2e089f51a48f402
Hammad <hammadyasub14@gmail.com>
2022-04-27 10:36:35 +0530

    final commit with all functionality

--- src/App.js
+++ src/App.js
@@ -4,20 +4,50 @@ class App extends React.Component {
   constructor() {
     super();
     this.state = {
-      listItem: ["Yes", "Probable Not", "Lets Nacho", "Maza aa gaya"],
+      listItem: [
+        "BMW",
+        "Mercedes",
+        "Audi",
+        "Volvo",
+        "Land Rover",
+        "Ferrari",
+        "Rolls Royce",
+      ],
       buttonValue: null,
+      hovering: false,
     };
   }
 
+  handleButtonHover = () => {
+    this.setState({
+      hovering: true,
+    });
+  };
+
+  handleListItemClick = (item) => {
+    this.setState({
+      buttonValue: item,
+      hovering: false,
+    });
+  };
+
+  handleArrowClick = () => {
+    let { hovering } = this.state;
+    let toggleHovering = hovering ? false : true;
+    this.setState({
+      hovering: toggleHovering,
+    });
+  };
+
   render() {
-    const { buttonValue, listItem } = this.state;
+    const { buttonValue, listItem, hovering } = this.state;
     return (
       <div className="App">
         <div id="dropdown-container">
-          <h1>Should you use a dropdown ?</h1>
-          <div id="dropdown-btn">
+          <h1>Let's choose your favourite car brand from a dropdown !</h1>
+          <div id="dropdown-btn" onMouseEnter={this.handleButtonHover}>
             <div id="btn-value">{buttonValue ? buttonValue : "Select"}</div>
-            <div id="open-dropdown">
+            <div id="open-dropdown" onClick={this.handleArrowClick}>
               <img
                 src="https://cdn-icons-png.flaticon.com/512/32/32195.png"
                 alt="open dropdown"
@@ -26,9 +56,19 @@ class App extends React.Component {
           </div>
 
           <div id="dropdown-list">
-            {listItem.map((item) => {
-              return <div className="list-item">{item}</div>;
-            })}
+            {hovering
+              ? listItem.map((item) => {
+                  return (
+                    <div
+                      className="list-item"
+                      onClick={() => this.handleListItemClick(item)}
+                      key={listItem.indexOf(item)}
+                    >
+                      <span>{item}</span>
+                    </div>
+                  );
+                })
+              : null}
           </div>
         </div>
       </div>
